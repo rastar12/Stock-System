cc56277b7a9b842832db9a91e831d7b63132b729
Eugene Chanzu <eugenechanzu@gmail.com>
2024-06-06 18:09:26 +0300

    2.6.1

--- client/src/components/stockList.jsx
+++ client/src/components/stockList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getStock } from '../services/stockService.js';
+import { getStock } from '../services/stockService.js'
 
 const StockList = () => {
   const [stockData, setStockData] = useState(null);
